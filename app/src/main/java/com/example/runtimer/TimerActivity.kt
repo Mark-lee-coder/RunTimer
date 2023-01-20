5d66ea4d45e5c80a14d6825ff94806b3c28511b7
LEONE <leonwahungu@gmail.com>
2023-01-20 16:32:59 +0300

    Added a pause button when timer is running

--- app/src/main/java/com/example/runtimer/TimerActivity.kt
+++ app/src/main/java/com/example/runtimer/TimerActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 import kotlinx.android.synthetic.main.activity_timer.*
